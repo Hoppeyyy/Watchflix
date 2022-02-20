f6471a37fa3d4172a4e8c533176f746eeb88b46b
joycechan <ychan119@my.bcit.ca>
2022-02-19 19:35:31 -0800

    Merge branch 'main' of https://github.com/Hoppeyyy/Watchflix into joyce

--- pages/testing.js
+++ pages/testing.js
@@ -1,20 +1,20 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import styled from 'styled-components';
-import React from 'react';
-import { useTheme } from "@/utils/provider";
-import ClickButton from '../comps/ClickButton';
+// import Head from 'next/head';
+// import { useRouter } from 'next/router';
+// import styled from 'styled-components';
+// import React from 'react';
+// import { useTheme } from "@/utils/provider";
+// import ClickButton from '../comps/ClickButton';
 
-=======
-import SubButton from '@/comps/SubButton';
-import CommentForm from '@/comps/CommentForm';
+// =======
+// import SubButton from '@/comps/SubButton';
+// import CommentForm from '@/comps/CommentForm';
 
 
-export default function Home() {
+// export default function Home() {
 
-  const {theme, setTheme} = useTheme();
+//   const {theme, setTheme} = useTheme();
   
-  return (
+//   return (
 
-  )
-}
+//   )
+// }
